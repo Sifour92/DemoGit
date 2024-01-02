da95a87daeaca8618486ea1ed7fa961c1054c89d
Aleksandr Bernatovich <sagder91@gmail.com>
2024-01-02 11:18:59 +0300

    add post

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,9 +1,7 @@
 package com.example.demo.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/demo")
@@ -12,5 +10,10 @@ public class DemoController {
     @GetMapping
     public String getAll(){
         return "aboba";
+    }
+
+    @PostMapping
+    public String postR(@RequestBody String aboba) {
+        return "biba";
     }
 }
